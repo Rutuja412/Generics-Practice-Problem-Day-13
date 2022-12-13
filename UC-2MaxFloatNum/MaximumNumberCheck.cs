39dff2389e30535cb51fe3672d2506bd3b4cf1ab
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 17:07:17 +0530

    [Rutuja]=Check Maximum Float Number

---
+++ UC-2MaxFloatNum/MaximumNumberCheck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_2MaxFloatNum
+{
+    public class MaximumNumberCheck
+    {
+        public static double MaximumFloatNumber(double firstvalue, double secondvalue, double thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber,second Number & third Number are same");
+        }
+    }
+}
+
---
+++ UC-2MaxFloatNum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_2MaxFloatNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            double doubleOutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 12.2, 10.2);
+            Console.WriteLine(doubleOutput);
+        }
+    }
+}
