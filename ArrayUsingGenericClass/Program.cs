5b699febf7d82a083706f48218beaff34cee709f
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 16:46:54 +0530

    [Rutuja]=Adding Array using Generic Class

---
+++ ArrayUsingGenericClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ArrayUsingGenericClass
+{
+
+    class Program
+    {
+        public static void toPrint<T>(T[] inputarray)
+        {
+            foreach (var element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("------------");
+        }
+
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArray = { 1.1, 2.2, 2.3, 2.4, 2.9 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+        }
+    }
+}
