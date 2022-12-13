aeb269cdcbd604b427de6d13ee91a7ed100ddc27
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 21:33:47 +0530

    [Rutuja]=Extend Maximum Number

---
+++ UC-4ExtendMax/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_4ExtendMax
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+            generic.PrintMaxValue();
+        }
+    }
+}
