a05ef854b668ecbf9811df428108a016a8e57c43
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 21:16:03 +0530

    [Rutuja]=Refactor all 3 to one generic method

---
+++ Refactor1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Refactor1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Number!!");
+            int[] Array = { 123, 464, 353, };
+            GenericMaximum<int> generic = new GenericMaximum<int>(Array);
+            generic.PrintMaxValue();
+
+            double[] doubleArray = { 1.1, 1.2, 1.3 };
+            GenericMaximum<double> genericMaximum = new GenericMaximum<double>(doubleArray);
+            genericMaximum.PrintMaxValue();
+
+            string[] stringArray = { "Apple", "Banana", "Pineapple" };
+            GenericMaximum<string> genericMaximum1 = new GenericMaximum<string>(stringArray);
+            genericMaximum1.PrintMaxValue();
+
+        }
+    }
+}
