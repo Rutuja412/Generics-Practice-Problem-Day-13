74db0ed4abce699dcf915b319cdea19fbd6d9324
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-13 16:58:06 +0530

    [Rutuja]=Check Maximum Integer

---
+++ UC1-MaxIntCheck/MaximumNumberCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1_MaxIntCheck
+{
+    public class MaximumNumberCheck
+    {
+        public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber,second Number & third Number are same");
+        }
+    }
+}
---
+++ UC1-MaxIntCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC1_MaxIntCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum number");
+
+            int output = MaximumNumberCheck.MaximumIntegerNumber(100, 200, 300);
+            Console.WriteLine(output);
+        }
+    }
+}
